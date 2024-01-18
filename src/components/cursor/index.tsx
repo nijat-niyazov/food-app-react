017d0f211f452f78b477be8c96cd7d209b4bc1db
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2024-01-18 17:21:16 +0400

    rmeoved shadcn

--- src/components/cursor/index.tsx
+++ src/components/cursor/index.tsx
@@ -6,7 +6,7 @@ const Console = () => {
         <li>H</li>
         <li>H</li>
         <li className="bg-primary absolute top-0 translate-y-1/2 rounded-full p-4">
-          T
+          H
         </li>
         <li>H</li>
         <li>H</li>
