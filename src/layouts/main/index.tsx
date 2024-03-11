2df4d6ebf913d0300ca99d73fee547e042d99e8c
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2024-03-11 21:49:12 +0400

    carousels merged to master

--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-main">
       {/* <div className="bg-text ">
         <div className="bg-white rounded-b-3xl pb-10 "> */}
 
